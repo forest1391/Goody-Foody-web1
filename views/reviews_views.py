c5e23fcb04185b2743de3e7242b22456d08c1741
annie <10756031>
2022-09-22 14:50:05 +0800

    till0922update

--- views/reviews_views.py
+++ views/reviews_views.py
@@ -13,7 +13,13 @@ def index(request):
 
 @user_login_required
 def analyze(request):
-    return render(request, 'analyze.html')
+    r = requests.get(
+        f'{root}/analyze/all/',
+        cookies={'sessionid': request.COOKIES['sessionid']}
+    )
+    result = r.json()
+    eatings = result['data']
+    return render(request, 'analyze.html', {'eatings': eatings})
 
 # @user_login_required
 # def coming(request):
@@ -51,6 +57,10 @@ def Userintroduction(request):
 def consult(request):
     return render(request, 'consult.html')
 
+@user_login_required
+def add_eating(request):
+    return render(request, 'add_eating.html')
+
 
 @user_login_required
 def menu(request):
@@ -73,6 +83,9 @@ def post(request):
         f'{root}/tag/all/',
         cookies={'sessionid': request.COOKIES['sessionid']}
     )
+
+
+
     result1 = r1.json()
     result2 = r2.json()
     # print(result)
@@ -86,7 +99,7 @@ def post(request):
 @user_login_required
 def add(request):
     if request.method =='GET':
-        return render(request, 'newpost.html')
+        return render(request, 'add_post.html')
 
     post_id = request.POST['post_id']
     title = request.POST['title']
@@ -104,6 +117,63 @@ def add(request):
         )
 
     return redirect('/community/')
+
+@user_login_required
+def add_tag(request):
+    if request.method =='GET':
+        return render(request, 'add_tag.html')
+
+    tag_id = request.POST['tag_id']
+    tag_name = request.POST['tag_name']
+    tag_type_id = request.POST['tag_type_id']
+
+    data = {
+        'tag_id':tag_id,
+        'tag_name':tag_name,
+        'tag_type_id':tag_type_id
+
+    }
+    r = requests.post(
+        f'{root}/tag/add/',
+        data=data
+        )
+
+    return redirect('/community/')
+
+@user_login_required
+def add_eating(request):
+    if request.method =='GET':
+        return render(request, 'add_eating.html')
+
+    eat_id = request.POST['eat_id']
+    account = request.POST['account']
+    menu_id = request.POST['menu_id']
+    eat_type_id = request.POST['eat_type_id']
+    date = request.POST['date']
+    kcal = request.POST['kcal']
+    carbohydrate = request.POST['carbohydrate']
+    fat = request.POST['fat']
+    protein = request.POST['protein']
+
+    data = {
+        'eat_id': eat_id,
+        'account': account,
+        'menu_id': menu_id,
+        'eat_type_id':eat_type_id,
+        'date': date,
+        'kcal':kcal,
+        'carbohydrate':carbohydrate,
+        'fat':fat,
+        'protein':protein
+
+    }
+    r = requests.post(
+        f'{root}/eating/add/',
+        data=data
+        )
+
+    return redirect('/analyze/')
+
 
 
 @user_login_required
@@ -128,7 +198,13 @@ def storeinformation(request):
 
 @user_login_required
 def map(request):
-    return render(request, 'map.html')
+    r = requests.get(
+        f'{root}/restaurant/all/',
+        cookies={'sessionid': request.COOKIES['sessionid']}
+    )
+    result = r.json()
+    restaurants = result['data']
+    return render(request, 'map.html', {'restaurants': restaurants})
 
 @user_login_required
 def member(request):
@@ -136,20 +212,20 @@ def member(request):
 
 @user_login_required
 def newpost(request):
-    return render(request, 'newpost.html')
+    return render(request, 'add_post.html')
 
 @user_login_required
 def verify_diet(request):
     return render(request, 'verify_diet.html')
 
-user_login_required
+@user_login_required
 def verify_store(request):
     return render(request, 'verify_store.html')
 
 # @user_login_required
 # def add(request):
 #     if request.method == 'GET':
-#         return render(request, 'newpost.html')
+#         return render(request, 'add_post.html')
 #
 #     post_id = request.POST['post_id']
 #     title = request.POST['title']
